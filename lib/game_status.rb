f0eb4c3836564de7ba35492d761045234caf1271
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-16 21:53:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,21 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], #Top row
+  [3, 4, 5], #Middle row
+  [6, 7, 8], #Bottom row
+  [0, 4, 8], # left to right Diagonal
+  [2, 4, 6], #right to left diagonal
+  [0, 3, 6], #left column
+  [1, 4, 7], #center column
+  [2, 5, 8] #right column
+]
+
+def won(board)
+  WIN_COMBINATIONS.each do |combination|
+    if condition.all? do|index| { board[index] == "X"}
+      break
+    end
+  end
+end
