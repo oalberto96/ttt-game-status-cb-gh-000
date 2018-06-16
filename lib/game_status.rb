dd4d2967b28ac2d5772ec03b1593b75725d28280
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-16 22:02:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |combination|
-    combination.select{|index|  board[index] == "X"}
+    if combination.select{|index|  board[index] == "X"}
+      break
   end
 end
